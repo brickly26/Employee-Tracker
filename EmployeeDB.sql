e7f2ba3cbb0a494be42aae372f4419b34706ac6a
Burak Aksu <sabburak26@gmail.com>
2021-08-17 19:06:01 -0400

    added checkTable function

--- EmployeeDB.sql
+++ EmployeeDB.sql
@@ -4,12 +4,12 @@ CREATE DATABASE Management;
 
 USE Management;
 
-CREATE TABLE department (
+CREATE TABLE departments (
   id INTEGER AUTO_INCREMENT NOT NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
   id INTEGER AUTO_INCREMENT NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INTEGER NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE role (
   FOREIGN KEY (department_id) REFERENCES department (id)
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
   id INTEGER AUTO_INCREMENT NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
--- EmployeeTracker.js
+++ EmployeeTracker.js
@@ -2,8 +2,55 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require('console.table');
 
-const addData = () => {
+const connection = mysql.createConnection({  
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'Baruch2021',
+  database: 'management'
+})
+
+const checkTable = (table) => {
+  connection.query('SELECT * FROM ?', {Table: table}, (err, res) => {
+    if (res) {
+      return false;
+    } else {
+      return true;
+    }
+  });
+}
+
+const addData = async () => {
   
+  const { userChoice } =  await inquirer.prompt([
+    {
+      type: "list",
+      message: "What table would you like to add data to?",
+      name: "userChoice",
+      choices: ["Employee", "Role", "Department"]
+    }
+  ])
+  switch (userChoice) {
+    case "Employee":
+      if(checkTable("roles")) {
+        addEmployee();
+      } else {
+        console.log("\nYou cannot added a Employee since there is no roles.\nTry adding a role first.\n")
+        addData();
+      }
+      break
+    case "Role":
+      if(checkTable("departments")) {
+        addRole();
+      } else {
+        console.log("\nYou cannot added a Role since there is no Departments.\nTry adding a Department first.\n")
+        addData();
+      }
+      break
+    case "Department":
+      addDepartment();
+      break
+  }
 }
 
 const viewData = () => {
@@ -27,7 +74,7 @@ const init = async () => {
     case "Add Data":
       addData();
       break
-    case "View DATA":
+    case "View Data":
       viewData();
       break
     case "Update Data":
@@ -35,5 +82,10 @@ const init = async () => {
       break
   }
 }
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('connect')
+});
 
 init();
